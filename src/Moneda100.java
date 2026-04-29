e873ba0d6529e66d8a10fdd981087b8fd3f43747
crist <araya.cama@gmail.com>
2026-04-28 23:12:26 -0400

    feat: se movio las clases de monedas en una nueva brench

--- src/Moneda100.java
+++
@@ -1,6 +0,0 @@
-public class Moneda100 extends Moneda {
-    @Override
-    public int getValor() {
-        return 100;
-    }
-}
--- src/Moneda1000.java
+++
@@ -1,6 +0,0 @@
-public class Moneda1000 extends Moneda {
-    @Override
-    public int getValor() {
-        return 1000;
-    }
-}
